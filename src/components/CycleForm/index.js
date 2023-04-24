8486a8f94fc43e04b28421ec87072f3f763a1c70
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-24 03:52:05 -0400

    form

--- src/components/CycleForm/index.js
+++ src/components/CycleForm/index.js
@@ -1,7 +1,15 @@
-import { useState } from "react-native";
+import React, { useState } from "react";
 
 function CycleForm ( {onSubmit} ) {
+    const [cycleLength, setCycleLength] = useState("");
+    const [periodLength, setPeriodLength] = useState("");
+    const [lastPeriod, setLastPeriod] = useState("");
 
+    return (
+        <form>
+            
+        </form>
+    );
 };
 
 export default CycleForm;
