73c6d23a40806d2510d1ce153eed55fbf7e11e48
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-24 03:40:11 -0400

    test

--- portfolio/App.js
+++ portfolio/App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There! Choose an option below:</Text>
-      <Button title="Edit Form" Button/>
+      <Text>Hello There! Choose an option below:</Text> <br></br>
+      <Button title="Edit Form" Button/> <br></br>
+      <Button title="My Calendar" Button/> <br></br> <br></br>
+      <Text>Select a Phase:</Text> <br></br>
+      <Button title="Menstruation" Button/> <br></br>
+      <Button title="Follicular Phase" Button/> <br></br>
+      <Button title="Ovulation" Button/> <br></br>
+      <Button title="Luteal Phase" Button/> <br></br>
       <StatusBar style="auto" />
     </View>
   );
