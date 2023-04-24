224eb5ccbb60fc362d9816fc9b6adb68e5d6a85f
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-24 03:45:17 -0400

    d

--- portfolio/App.js
+++ portfolio/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello There! Choose an option below:</Text> <br></br>
+      <Text>Welcome Back! Choose an option below:</Text> <br></br>
       <Button title="Edit Form" Button/> <br></br>
       <Button title="My Calendar" Button/> <br></br> <br></br>
       <Text>Select a Phase:</Text> <br></br>
