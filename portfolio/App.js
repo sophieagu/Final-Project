0872ad3714269d0128738982064d748cc9d0ba3b
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-24 23:47:44 -0400

    m

--- portfolio/App.js
+++ portfolio/App.js
@@ -4,17 +4,17 @@ import { NavigationContainer, useNavigation } from '@react-navigation/native';
 //import CycleForm from '/components/CycleForm';
 
 export default function App() {
-  const Navigation = useNavigation();
+  //const Navigation = useNavigation();
   return (
     <View style = {styles.container}>
         <Text>Welcome Back! Choose an option below:</Text> <br></br>
-        <Button title="Edit Form" onPress={() => Navigation.navigate('CycleForm')} Button/> <br></br>
-        <Button title="My Calendar"  onPress={() => Navigation.navigate('CalendarScreen')} Button/> <br></br> <br></br>
+        <Button title="Edit Form" Button/> <br></br>
+        <Button title="My Calendar"  Button/> <br></br> <br></br>
         <Text>Select a Phase:</Text> <br></br>
-        <Button title="Menstruation"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
-        <Button title="Follicular Phase"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
-        <Button title="Ovulation" onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
-        <Button title="Luteal Phase" onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
+        <Button title="Menstruation"  Button/> <br></br>
+        <Button title="Follicular Phase" Button/> <br></br>
+        <Button title="Ovulation" Button/> <br></br>
+        <Button title="Luteal Phase" Button/> <br></br>
         <StatusBar style="auto" />
       </View>
   );
