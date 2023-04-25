bf342885dc8f18693f20ba58a45fc1c0468d7618
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-24 20:34:18 -0400

    ,

--- portfolio/src/components/CycleForm/index.js
+++ portfolio/src/components/CycleForm/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {NavigationContainer, useNavigation} from '@react-navigation/native'
 
 function CycleForm ( {onSubmit} ) {
     const [cycleLength, setCycleLength] = useState("");
