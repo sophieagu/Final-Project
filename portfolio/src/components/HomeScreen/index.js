c7129f40c46710d7a4d7a2662eea89b1ee321c8d
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-26 00:42:18 -0400

    prep

--- portfolio/src/components/HomeScreen/index.js
+++ portfolio/src/components/HomeScreen/index.js
@@ -1,2 +1,31 @@
 import React, {useState} from "react";
-import { Text } from ""
+import { Text, Button, View } from "react-native";
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+const Home = () => {
+    const Navigation = useNavigation();
+    return (
+        <View style = {styles.container}>
+            <Text>Welcome Back! Choose an option below:</Text> <br></br>
+            <Button title="Edit Form" onPress={() => Navigation.navigate('CycleForm')} Button/> <br></br>
+            <Button title="My Calendar"  onPress={() => Navigation.navigate('CalendarScreen')} Button/> <br></br> <br></br>
+            <Text>Select a Phase:</Text> <br></br>
+            <Button title="Menstruation"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>                <Button title="Follicular Phase"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
+            <Button title="Ovulation" onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
+            <Button title="Luteal Phase" onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
+
+export default Home;
