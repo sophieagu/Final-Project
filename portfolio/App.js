c924446159d5fa72876398ecd85d0701e5809cdf
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-26 01:50:21 -0400

    ,

--- portfolio/App.js
+++ portfolio/App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CycleForm from './src/components/CycleForm';
 import HomeScreen from './src/components/HomeScreen';
 import PhaseScreen from './src/components/PhaseScreen';
 import CalendarScreen from './src/components/CalendarScreen';
 
 function App() {
-  const Stack = useNavigation();
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
