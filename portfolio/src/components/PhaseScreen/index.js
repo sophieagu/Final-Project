c32905fafc7e04569df36d2f0fc2f91869d92077
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-26 01:53:40 -0400

    phasescreen set up

---
+++ portfolio/src/components/PhaseScreen/index.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+const PhaseScreen = () => {
+
+    return (
+        <Text>Hello</Text>
+    )
+}
+
+export default PhaseScreen;
