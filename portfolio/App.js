4c9af674d62c2a1f87b8e97e3b66440321599ab0
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-26 00:58:05 -0400

    ,

--- portfolio/App.js
+++ portfolio/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import CycleForm from './src/components/CycleForm';
-import HomeScreen from '/components/HomeScreen';
+import HomeScreen from './components/HomeScreen';
 import PhaseScreen from './components/PhaseScreen';
 import CalendarScreen from './components/CalendarScreen';
 
