81d547c4ed9fd9adc19acd964311b4326c7d386e
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 01:38:11 -0400

    setup

--- portfolio/src/components/CycleForm/index.js
+++ portfolio/src/components/CycleForm/index.js
@@ -15,7 +15,27 @@ const CycleForm = ( {onSubmit} ) => {
 
     return (
         <View style={styles.container}>
-
+            <Text style={styles.label}>Cycle Length:</Text>
+            <TextInput
+            style={styles.input}
+            keyboardType='numeric'
+            value={cycleLength}
+            onChangeText={(text) => setCycleLength(text)}
+            />
+            <Text style={styles.label}>Period Length:</Text>
+            <TextInput
+            style={styles.input}
+            keyboardType='numeric'
+            value={periodLength}
+            onChangeText={(text) => setPeriodLength(text)}
+            />
+            <Text style={styles.label}>Start of last period:</Text>
+            <TextInput>
+            style={styles.input}
+            keyboardType='numeric'
+            value={lastPeriodStart}
+            onChangeText={(text) => setLastPeriodStart(text)}
+            </TextInput>
         </View>
     );
 };
@@ -27,6 +47,21 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        width: '80%',
+        height: 40,
+        padding: 10,
+        marginBottom: 10,
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 10,
+        marginBottom: 5,
+      }
   });
 
 export default CycleForm;
