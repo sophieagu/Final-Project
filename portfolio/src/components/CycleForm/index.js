5b958e5476e8575eee6803d8f9d089aa2f0c2737
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 01:50:37 -0400

    pls

--- portfolio/src/components/CycleForm/index.js
+++ portfolio/src/components/CycleForm/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const CycleForm = ( {onSubmit} ) => {
+const CycleForm = () => {
     const [cycleLength, setCycleLength] = useState("");
     const [periodLength, setPeriodLength] = useState("");
     const [lastPeriodStart, setLastPeriodStart] = useState("");
