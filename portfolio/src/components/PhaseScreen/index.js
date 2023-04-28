65f06dac2c2e89b888de6307b87172c507af0d88
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 19:38:44 -0400

    ,

--- portfolio/src/components/PhaseScreen/index.js
+++ portfolio/src/components/PhaseScreen/index.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, Button, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
 const PhaseScreen = () => {
+    const [currentPhase, setCurrentPhase] = useState("");
 
     return (
         <Text>Hello, you made it to the phasescreen</Text>
