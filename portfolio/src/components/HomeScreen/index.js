f3d4f12a6466dd3f6992a4f47266e2021659bbd5
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 19:46:02 -0400

    wb now

--- portfolio/src/components/HomeScreen/index.js
+++ portfolio/src/components/HomeScreen/index.js
@@ -3,16 +3,16 @@ import React, {useState} from "react";
 import { StyleSheet, Text, Button, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
-const HomeScreen = () => {
+const HomeScreen = ( {route} ) => {
     const Navigation = useNavigation();
     const { avgPeriodLength, avgCycleLength } = route.params || {};
-    
+
     return (
         <View style = {styles.container}>
-            <Text style = {bolds.container}>Welcome Back! Choose an option below:</Text> <br></br>
+            <Text style = {styles.titles}>Welcome Back! Choose an option below:</Text> <br></br>
             <Button title="Update Cycle Information" onPress={() => Navigation.navigate('CycleForm')} Button/> <br></br>
             <Button title="My Calendar"  onPress={() => Navigation.navigate('CalendarScreen')} Button/> <br></br> <br></br>
-            <Text style = {bolds.container}>Select a Phase:</Text> <br></br>
+            <Text style = {styles.titles}>Select a Phase:</Text> <br></br>
             <Button title="Menstruation"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>                
             <Button title="Follicular Phase"  onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
             <Button title="Ovulation" onPress={() => Navigation.navigate('PhaseScreen')} Button/> <br></br>
@@ -29,10 +29,7 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
-  });
-
-  const bolds = StyleSheet.create({
-    container: {
+    titles: {
       fontWeight: "bold",
       fontSize: "18pt",
       backgroundColor: '#fff',
@@ -40,6 +37,8 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
   });
+
+
   
 
 export default HomeScreen;
