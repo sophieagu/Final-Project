5a912186bb0529d58708c3662797100dc7dfecef
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 20:19:43 -0400

    setup

--- portfolio/src/components/PhaseScreen/index.js
+++ portfolio/src/components/PhaseScreen/index.js
@@ -2,8 +2,9 @@ import React, {useState} from "react";
 import { StyleSheet, Text, Button, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
-const PhaseScreen = () => {
-    const [currentPhase, setCurrentPhase] = useState("");
+const PhaseScreen = ( {route} ) => {
+    const { phase, avgPeriodLength, avgCycleLength } = route.params;
+    const [currentPhase, setCurrentPhase] = useState(phase);
 
     return (
         <Text>Hello, you made it to the phasescreen</Text>
