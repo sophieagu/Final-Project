71d2a03a7726b0d8bea1ebb49f57cdd4e9f04a4e
sophieagu <sophieagu12@knights.ucf.edu>
2023-04-28 20:38:51 -0400

    type and title

--- portfolio/src/components/PhaseScreen/index.js
+++ portfolio/src/components/PhaseScreen/index.js
@@ -4,8 +4,35 @@ import { useNavigation } from '@react-navigation/native';
 
 const PhaseScreen = ( {route} ) => {
     const { phase, avgPeriodLength, avgCycleLength } = route.params;
-    const [currentPhase, setCurrentPhase] = useState(phase);
+    const [currentPhase, setCurrentPhase] = useState("");
 
+    const menstruationWorkouts = [
+        {title: "Walking", type: "Duration"},
+        {title: "Yoga", type: "Duration"},
+        {title: "Pilates", type: "Duration"}
+    ];
+
+    const follicularWorkouts = [
+        {title: "Jumping Jacks", type: "Repetition"},
+        {title: "Pushups", type: "Repetition"},
+        {title: "BenchPress", type: "Repetition"},
+        {title: "Squats", type: "Repetition"},
+        {title: "Running", type: "Duration"}
+    ];
+
+    const ovulationWorkouts = [
+        {title: "Running", type: "Duration"},
+        {title: "Plank", type: "Duration"},
+        {ttle: "Pushups", type: "Repetition"},
+        {title: "Jumping Jacks", type: "Repetition"}
+    ];
+
+    const lutealWorkouts = [
+        {title:"Jogging", type: "Duration"},
+        {title: "Walking", type: "Duration"},
+        {title: "Yoga", type: "Duration"},
+        {title: "Pilates", type: "Duration"}
+    ]
     return (
         <Text>Hello, you made it to the phasescreen</Text>
     )
